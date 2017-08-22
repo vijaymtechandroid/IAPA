de8ad5b26dc64fa1435784ff734207ce37d13c10
vijaymtechandroid <vijaymtechandroid@gmail.com>
2017-08-22 16:02:04 +0530

    Actions are performed

--- app/src/main/java/com/medgini/testforgithub/MainActivity.java
+++ app/src/main/java/com/medgini/testforgithub/MainActivity.java
@@ -3,22 +3,30 @@ package com.medgini.testforgithub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView text;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        img = (ImageView)findViewById(R.id.img);
         text = (TextView)findViewById(R.id.txtviw);
         text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Some thing new ", Toast.LENGTH_SHORT).show();
+            }
+        });
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                img.setVisibility(View.GONE);
             }
         });
 
