f7aba5154c726ae6a6a3ab83f46273a0fdd61012
vijaymtechandroid <vijaymtechandroid@gmail.com>
2017-08-22 15:43:21 +0530

    Images are added

--- app/src/main/java/com/medgini/testforgithub/MainActivity.java
+++ app/src/main/java/com/medgini/testforgithub/MainActivity.java
@@ -2,12 +2,25 @@ package com.medgini.testforgithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView)findViewById(R.id.txtviw);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Some thing new ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
